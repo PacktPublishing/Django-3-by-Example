0533cc678c77e3a259b33113592502cd80d9476b
Antonio Melé <antonio.mele@gmail.com>
2019-10-15 09:21:08 +0200

    Add Chapter01 code

---
+++ Chapter01/mysite/blog/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                          .filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+    author = models.ForeignKey(User,
+                              on_delete=models.CASCADE,
+                              related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day, self.slug])
