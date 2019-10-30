053bf7052bf62cd2dde1a21c04166233f58dc383
Antonio Melé <antonio.mele@gmail.com>
2019-10-30 19:54:28 +0100

    removed duplicated import

--- Chapter01/mysite/blog/views.py
+++ Chapter01/mysite/blog/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
