7cab9ecd4f092e515e8c0b9b913e1a1d2f5fba8b
Antonio Melé <antonio.mele@gmail.com>
2019-11-18 09:14:06 +0100

    Add Chapter 04 code

---
+++ Chapter04/bookmarks/account/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    # previous login view
+    # path('login/', views.user_login, name='login'),
+
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    # change password urls
+    # path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),  name='password_change_done'),
+
+    # reset password urls
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
+]
---
+++ Chapter04/bookmarks/bookmarks/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('account/', include('account.urls')),
+    path('social-auth/', include('social_django.urls', namespace='social')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
