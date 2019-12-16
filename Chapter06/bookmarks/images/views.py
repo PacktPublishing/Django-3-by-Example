f03717a29b7aab545f4bfa9218c468fcce41e216
Antonio Melé <antonio.mele@gmail.com>
2019-12-16 17:44:21 +0100

    Changed from StrictRedis to Redis wich is now used in redis-py >=3.0

--- Chapter06/bookmarks/images/views.py
+++ Chapter06/bookmarks/images/views.py
@@ -16,9 +16,9 @@ import redis
 from django.conf import settings
 
 # connect to redis
-r = redis.StrictRedis(host=settings.REDIS_HOST,
-                      port=settings.REDIS_PORT,
-                      db=settings.REDIS_DB)
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 @login_required
