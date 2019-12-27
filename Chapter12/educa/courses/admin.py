1d5ae719bb52f19f3cb23f29691c4df72bb3271c
Antonio Melé <antonio.mele@gmail.com>
2019-12-27 20:33:09 +0100

    Add Chapter 12 code

---
+++ Chapter12/educa/courses/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Subject, Course, Module
+from django.contrib import admin
+
+# use memcache admin index site
+admin.site.index_template = 'memcache_status/admin_index.html'
+
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+class ModuleInline(admin.StackedInline):
+    model = Module
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ['title', 'subject', 'created']
+    list_filter = ['created', 'subject']
+    search_fields = ['title', 'overview']
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [ModuleInline]
---
+++ Chapter12/educa/educa/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
+from courses.views import CourseListView
+
+urlpatterns = [
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+    path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
+    path('students/', include('students.urls')),
+    path('api/', include('courses.api.urls', namespace='api')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
