b88f84a7dd97eb30d6c9eebca617e00e4a55610c
Antonio Melé <antonio.mele@gmail.com>
2020-02-10 13:34:09 +0100

    Add Chapter 13 code

---
+++ Chapter13/educa/chat/routing.py
@@ -0,0 +1,6 @@
+from django.urls import re_path
+from . import consumers
+
+websocket_urlpatterns = [
+    re_path(r'ws/chat/room/(?P<course_id>\d+)/$', consumers.ChatConsumer),
+]
---
+++ Chapter13/educa/courses/templatetags/course.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def model_name(obj):
+    try:
+        return obj._meta.model_name
+    except AttributeError:
+        return None
