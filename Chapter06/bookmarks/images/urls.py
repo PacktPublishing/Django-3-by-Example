979515b105d49f7b21e3658d9dc581f0ad9e53e3
Antonio Melé <antonio.mele@gmail.com>
2020-02-24 12:09:50 +0100

    Fix URL name for the image_ranking view

--- Chapter06/bookmarks/images/urls.py
+++ Chapter06/bookmarks/images/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('detail/<int:id>/<slug:slug>/', views.image_detail, name='detail'),
     path('like/', views.image_like, name='like'),
     path('', views.image_list, name='list'),
-    path('ranking/', views.image_ranking, name='create'),
+    path('ranking/', views.image_ranking, name='ranking'),
 ]
