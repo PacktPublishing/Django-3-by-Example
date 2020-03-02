851392f30c5b6533a93f53d315a4971f6f12a6bf
Antonio Melé <antonio.mele@gmail.com>
2020-03-02 21:04:35 +0100

    Add payment_completed task

---
+++ Chapter09/myshop/payment/tasks.py
@@ -0,0 +1,36 @@
+from io import BytesIO
+from celery import task
+import weasyprint
+from django.template.loader import render_to_string
+from django.core.mail import EmailMessage
+from django.conf import settings
+from orders.models import Order
+
+
+@task
+def payment_completed(order_id):
+    """
+    Task to send an e-mail notification when an order is
+    successfully created.
+    """
+    order = Order.objects.get(id=order_id)
+
+    # create invoice e-mail
+    subject = f'My Shop - EE Invoice no. {order.id}'
+    message = 'Please, find attached the invoice for your recent purchase.'
+    email = EmailMessage(subject,
+                         message,
+                         'admin@myshop.com',
+                         [order.email])
+    # generate PDF
+    html = render_to_string('orders/order/pdf.html', {'order': order})
+    out = BytesIO()
+    stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
+    weasyprint.HTML(string=html).write_pdf(out,
+                                           stylesheets=stylesheets)
+    # attach PDF file
+    email.attach(f'order_{order.id}.pdf',
+                 out.getvalue(),
+                 'application/pdf')
+    # send e-mail
+    email.send()
--- Chapter09/myshop/payment/views.py
+++ Chapter09/myshop/payment/views.py
@@ -1,11 +1,8 @@
 import braintree
-import weasyprint
-from io import BytesIO
 from django.shortcuts import render, redirect, get_object_or_404
-from django.template.loader import render_to_string
-from django.core.mail import EmailMessage
 from django.conf import settings
 from orders.models import Order
+from .tasks import payment_completed
 
 
 # instantiate Braintree payment gateway
@@ -34,27 +31,8 @@ def payment_process(request):
             # store the unique transaction id
             order.braintree_id = result.transaction.id
             order.save()
-            # create invoice e-mail
-            subject = f'My Shop - Invoice no. {order.id}'
-            message = 'Please, find attached the invoice for your recent\
-            purchase.'
-            email = EmailMessage(subject,
-                                 message,
-                                 'admin@myshop.com',
-                                 [order.email])
-            # generate PDF
-            html = render_to_string('orders/order/pdf.html', {'order': order})
-            out = BytesIO()
-            stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')]
-            weasyprint.HTML(string=html).write_pdf(out,
-                                                   stylesheets=stylesheets)
-            # attach PDF file
-            email.attach(f'order_{order.id}.pdf',
-                         out.getvalue(),
-                         'application/pdf')
-            # send e-mail
-            email.send()
-
+            # launch asynchronous task
+            payment_completed.delay(order.id)
             return redirect('payment:done')
         else:
             return redirect('payment:canceled')
