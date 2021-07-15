611ccec37b7c1596bdcc8a2069c75c3f81f9874e
Frolov Roman <frolloff1@yandex.ru>
2021-07-15 11:05:28 +0300

    Update rounting.py for Channels v3
    add as_asgi() classmethod to re_path. This is similar to Django’s as_view(), which plays the same role for per-request Django view instances.

--- Chapter13/educa/chat/routing.py
+++ Chapter13/educa/chat/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/room/(?P<course_id>\d+)/$', consumers.ChatConsumer),
+    re_path(r'ws/chat/room/(?P<course_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
 ]
